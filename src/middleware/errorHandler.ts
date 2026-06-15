0258316b715450ca363798685afdb19ecedfcd3a
Daria <darakolmogorceva@gmail.com>
2026-06-15 23:54:56 +0300

    fixup! add middleware layer

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+export function errorHandler(err: any, _req: Request, res: Response, _next: NextFunction) {
     console.error(err);
     res.status(500).json({ error: 'Internal Server Error' });
 }
