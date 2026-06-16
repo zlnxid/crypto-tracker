16eca03145a930dfc5811ccb486abc0f2b6e65ff
Daria <darakolmogorceva@gmail.com>
2026-06-16 22:30:21 +0300

    move auth middleware to index router

--- src/routes/prices.ts
+++ src/routes/prices.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import { priceController } from '../controllers/priceController';
-import { auth } from '../middleware/auth';
 
 const router = Router();
 
-router.get('/current/:symbol', auth, priceController.getCurrent);
-router.get('/history/:symbol', auth, priceController.getHistoryFromBinance);
-router.get('/stored/history/:coinId', auth, priceController.getHistory);
-router.get('/stored/latest/:coinId', auth, priceController.getLatest);
+router.get('/current/:symbol', priceController.getCurrent);
+router.get('/history/:symbol', priceController.getHistoryFromBinance);
+router.get('/stored/history/:coinId', priceController.getHistory);
+router.get('/stored/latest/:coinId', priceController.getLatest);
 
 export default router;
